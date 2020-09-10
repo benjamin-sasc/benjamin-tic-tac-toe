92bffdd0106bf8d8c90774df39eb03c8c25075ee
benjamin-sasc <benjamin-sasc@dataindependence.net>
2020-09-09 19:21:57 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 var tdElements = document.getElementsByTagName('TD');
 
-for(var tdElement of tdElements){
-
+for(var tdElement of tdElements) {
+  tdElement. addEventListener('click', handleClick);
 }
+
+function handleClick(event) {
+  var target = event.target;
+  target.innerHTML = 'X';
+} 
